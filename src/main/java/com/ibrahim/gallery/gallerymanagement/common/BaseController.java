f1695c3cc316e5205e02ca40229844b0d54e68c7
ibrahim4851 <erogluih2001@gmail.com>
2023-06-18 18:23:00 +0300

    car package added

--- src/main/java/com/ibrahim/gallery/gallerymanagement/common/BaseController.java
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/common/BaseController.java
@@ -1,4 +1,62 @@
 package com.ibrahim.gallery.gallerymanagement.common;
 
-public class BaseController {
+import com.ibrahim.gallery.gallerymanagement.common.entity.BaseEntity;
+import com.ibrahim.gallery.gallerymanagement.common.mapper.BaseMapper;
+import com.ibrahim.gallery.gallerymanagement.common.service.BaseService;
+import com.ibrahim.gallery.gallerymanagement.common.util.BaseDTOUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Component
+public abstract class BaseController<Entity extends BaseEntity, DTO, Service extends BaseService<Entity, ID>, Mapper extends BaseMapper<Entity, ID>, ID> {
+
+    protected abstract Service getService();
+
+    protected abstract Mapper getMapper();
+
+    @PostMapping
+    public DTO save(@RequestBody @Validated DTO dto){
+        Entity entity = getService().save(getMapper().toEntity(dto));
+        DTO responseDTO = getMapper().toDTO(entity);
+        return responseDTO;
+    }
+
+    @PutMapping({"/{id}"})
+    public DTO put(@PathVariable("id") ID id, @RequestBody @Validated DTO dto) {
+
+        Entity existEntity = getService().get(id);
+        Entity newEntity = getMapper().toEntity(dto);
+
+        BaseDTOUtil.setBaseEntityProperty(existEntity, newEntity);
+
+        Entity putServiceResult = getService().put(newEntity);
+
+        DTO responseDTO = getMapper().toDTO(putServiceResult);
+
+        return responseDTO;
+    }
+
+    @GetMapping("/{id}")
+    public DTO get(@PathVariable ID id) {
+        DTO dto = getMapper().toDTO(getService().get(id));
+        return dto;
+    }
+
+    @DeleteMapping({"/{id}"})
+    public Boolean delete(@PathVariable("id") ID id) {
+        getService().delete(id);
+        return Boolean.TRUE;
+    }
+
+    @GetMapping
+    public List<DTO> findAll() {
+        List<Entity> entities = getService().findAll();
+        List<DTO> dtos = getMapper().toListDTO(entities);
+        return dtos;
+    }
+
 }
