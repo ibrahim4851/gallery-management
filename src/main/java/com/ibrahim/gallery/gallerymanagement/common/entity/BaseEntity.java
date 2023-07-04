d05919dcf5a8608c2f82720715f0686d2a2bbb5b
ibrahim4851 <erogluih2001@gmail.com>
2023-07-04 23:56:24 +0300

    car package update

--- src/main/java/com/ibrahim/gallery/gallerymanagement/common/entity/BaseEntity.java
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/common/entity/BaseEntity.java
@@ -8,8 +8,10 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -19,16 +21,23 @@ import java.util.Date;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 8573078053061101955L;
+
     @CreatedDate
+    @Column(name = "created_at")
     private Date createdAt;
 
     @CreatedBy
+    @Column(name = "created_by_user_id")
     private String createdByUserId;
 
     @LastModifiedDate
+    @Column(name = "last_modified_at")
     private Date lastModifiedAt;
 
     @LastModifiedBy
+    @Column(name = "last_modified_by_user_id")
     private String lastModifiedByUserId;
 
 }
