7abd77c0cf97c16d06185dce373a63286cb40c3a
ibrahim4851 <erogluih2001@gmail.com>
2023-07-15 12:26:00 +0300

    update

---
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/carchassis/dto/ChassisDTO.java
@@ -0,0 +1,15 @@
+package com.ibrahim.gallery.gallerymanagement.carchassis.dto;
+
+import com.ibrahim.gallery.gallerymanagement.common.dto.BaseDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChassisDTO extends BaseDTO {
+
+    private Long id;
+
+    private String name;
+
+}
