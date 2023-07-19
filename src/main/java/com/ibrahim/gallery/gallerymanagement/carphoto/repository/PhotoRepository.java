73b520c005f93df68524e4334d6faadfcb541787
ibrahim4851 <erogluih2001@gmail.com>
2023-07-19 04:00:00 +0000

    update

---
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/carphoto/repository/PhotoRepository.java
@@ -0,0 +1,7 @@
+﻿package com.ibrahim.gallery.gallerymanagement.carphoto.repository;
+
+import com.ibrahim.gallery.gallerymanagement.carphoto.entity.Photo;
+import com.ibrahim.gallery.gallerymanagement.common.repo.BaseRepository;
+
+public interface PhotoRepository extends BaseRepository<Photo, Long> {
+}
