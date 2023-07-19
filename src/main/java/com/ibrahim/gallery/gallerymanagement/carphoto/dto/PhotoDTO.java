1f6e607835011aecabedebd48a753567bd059a64
ibrahim4851 <erogluih2001@gmail.com>
2023-07-19 03:00:00 +0000

    update

---
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/carphoto/dto/PhotoDTO.java
@@ -0,0 +1,18 @@
+﻿package com.ibrahim.gallery.gallerymanagement.carphoto.dto;
+
+import com.ibrahim.gallery.gallerymanagement.common.dto.BaseDTO;
+import com.ibrahim.gallery.gallerymanagement.common.dto.ItemDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhotoDTO extends BaseDTO {
+
+    private Long id;
+
+    private ItemDTO car;
+
+    private String url;
+
+}
