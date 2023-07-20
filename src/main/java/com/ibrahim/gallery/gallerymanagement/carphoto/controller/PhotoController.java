ea3ef6d831e0f58cd817ca1dac9d0f77cdc12093
ibrahim4851 <erogluih2001@gmail.com>
2023-07-20 15:19:28 +0300

    update

---
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/carphoto/controller/PhotoController.java
@@ -0,0 +1,36 @@
+﻿package com.ibrahim.gallery.gallerymanagement.carphoto.controller;
+
+import com.ibrahim.gallery.gallerymanagement.carphoto.dto.PhotoDTO;
+import com.ibrahim.gallery.gallerymanagement.carphoto.entity.Photo;
+import com.ibrahim.gallery.gallerymanagement.carphoto.mapper.PhotoDtoMapper;
+import com.ibrahim.gallery.gallerymanagement.carphoto.service.PhotoService;
+import com.ibrahim.gallery.gallerymanagement.common.controller.BaseController;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("photos")
+public class PhotoController extends BaseController<Photo, PhotoDTO, PhotoService, PhotoDtoMapper, Long> {
+
+    private final PhotoService photoService;
+
+    private final  PhotoDtoMapper photoDtoMapper;
+
+    public PhotoController(MessageSource messageSource, PhotoService photoService, PhotoDtoMapper photoDtoMapper) {
+        super(messageSource);
+        this.photoService = photoService;
+        this.photoDtoMapper = photoDtoMapper;
+    }
+
+
+    @Override
+    protected PhotoService getService() {
+        return photoService;
+    }
+
+    @Override
+    protected PhotoDtoMapper getMapper() {
+        return photoDtoMapper;
+    }
+}
