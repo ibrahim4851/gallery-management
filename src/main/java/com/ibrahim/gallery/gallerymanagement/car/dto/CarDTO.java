6329126f4208995e8bc8ba7e428d34fc274b02ef
ibrahim4851 <erogluih2001@gmail.com>
2023-07-21 18:04:56 +0300

    update

--- src/main/java/com/ibrahim/gallery/gallerymanagement/car/dto/CarDTO.java
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/car/dto/CarDTO.java
@@ -1,11 +1,14 @@
 package com.ibrahim.gallery.gallerymanagement.car.dto;
 
 import com.ibrahim.gallery.gallerymanagement.carbrand.dto.CarBrandDTO;
+import com.ibrahim.gallery.gallerymanagement.carphoto.dto.PhotoDTO;
 import com.ibrahim.gallery.gallerymanagement.common.dto.BaseDTO;
+import com.ibrahim.gallery.gallerymanagement.common.dto.ItemDTO;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,6 +18,12 @@ public class CarDTO extends BaseDTO {
 
     @NotNull(message = "Brand can't be null")
     private CarBrandDTO brand;
+
+    @NotNull(message = "Series Can't be null")
+    private ItemDTO carSeries;
+
+    @NotNull(message = "At least one photo should be provided")
+    private List<PhotoDTO> photos;
 
     @NotNull(message = "Model can't be null")
     private String carModel;
