ec750018f8ffb1bd5670768a15ac21f52cc6b4d6
ibrahim4851 <erogluih2001@gmail.com>
2023-07-22 04:00:00 +0000

    update

---
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/carphoto/mapper/CarPhotoItemDTOMapper.java
@@ -0,0 +1,15 @@
+﻿package com.ibrahim.gallery.gallerymanagement.carphoto.mapper;
+
+import com.ibrahim.gallery.gallerymanagement.carphoto.entity.Photo;
+import com.ibrahim.gallery.gallerymanagement.common.dto.ItemDTO;
+import com.ibrahim.gallery.gallerymanagement.common.mapper.BaseMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CarPhotoItemDTOMapper extends BaseMapper<Photo, ItemDTO> {
+
+    @Mapping(source = "photoUrl", target = "text")
+    @Override
+    ItemDTO toDTO(Photo photo);
+}
