ae90fc2a8d318c40b2e62fb18d7f5bf78484eff6
ibrahim4851 <erogluih2001@gmail.com>
2023-07-23 04:00:00 +0000

    update

--- src/main/java/com/ibrahim/gallery/gallerymanagement/carphoto/service/PhotoService.java
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/carphoto/service/PhotoService.java
@@ -4,6 +4,7 @@ import com.ibrahim.gallery.gallerymanagement.carphoto.entity.Photo;
 import com.ibrahim.gallery.gallerymanagement.carphoto.repository.PhotoRepository;
 import com.ibrahim.gallery.gallerymanagement.common.repo.BaseRepository;
 import com.ibrahim.gallery.gallerymanagement.common.service.BaseService;
+import com.ibrahim.gallery.gallerymanagement.common.service.BaseServiceResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,10 @@ public class PhotoService extends BaseService<Photo, Long> {
     @Override
     public BaseRepository<Photo, Long> getRepository() {
         return photoRepository;
+    }
+
+    @Override
+    public BaseServiceResult<Photo> save(Photo photo) {
+        return super.save(photo);
     }
 }
