5224cfa6405d1cf61f11d0560b595ad28152dbb2
ibrahim4851 <erogluih2001@gmail.com>
2023-08-03 04:00:00 +0000

    update

---
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/attachment/repository/AttachmentRepository.java
@@ -0,0 +1,7 @@
+package com.ibrahim.gallery.gallerymanagement.attachment.repository;
+
+import com.ibrahim.gallery.gallerymanagement.attachment.entity.Attachment;
+import com.ibrahim.gallery.gallerymanagement.common.repo.BaseRepository;
+
+public interface AttachmentRepository extends BaseRepository<Attachment, Long> {
+}
