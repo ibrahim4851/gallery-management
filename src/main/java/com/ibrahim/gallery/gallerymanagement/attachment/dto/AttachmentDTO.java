0f1ea6e7d5ebfa7a0d608ec6d4224561c69546a8
ibrahim4851 <erogluih2001@gmail.com>
2023-08-06 00:02:24 +0300

    update

--- src/main/java/com/ibrahim/gallery/gallerymanagement/attachment/dto/AttachmentDTO.java
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/attachment/dto/AttachmentDTO.java
@@ -1,6 +1,19 @@
 package com.ibrahim.gallery.gallerymanagement.attachment.dto;
 
+import com.ibrahim.gallery.gallerymanagement.car.entity.Car;
+import com.ibrahim.gallery.gallerymanagement.carphoto.entity.Photo;
 import com.ibrahim.gallery.gallerymanagement.common.dto.BaseDTO;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class AttachmentDTO extends BaseDTO {
+
+    private Long id;
+
+    private Car carId;
+
+    private Photo photoId;
+
 }
--- src/main/java/com/ibrahim/gallery/gallerymanagement/attachment/mapper/AttachmentDtoMapper.java
+++ src/main/java/com/ibrahim/gallery/gallerymanagement/attachment/mapper/AttachmentDtoMapper.java
@@ -3,6 +3,8 @@ package com.ibrahim.gallery.gallerymanagement.attachment.mapper;
 import com.ibrahim.gallery.gallerymanagement.attachment.dto.AttachmentDTO;
 import com.ibrahim.gallery.gallerymanagement.attachment.entity.Attachment;
 import com.ibrahim.gallery.gallerymanagement.common.mapper.BaseMapper;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface AttachmentDtoMapper extends BaseMapper<Attachment, AttachmentDTO> {
 }
